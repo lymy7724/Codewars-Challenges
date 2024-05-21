372f0776db254917b71e5531cddf7e289bf99722
Ly My Y <lymy7724@gmail.com>
2024-05-21 11:43:42 -0400

    completed day1
    completed challenges for day 1

---
+++ Day 1 Challenges/three.js
@@ -0,0 +1,20 @@
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+
+function basicOp(operation, value1, value2) {
+  if (operation === "+") {
+    return value1 + value2;
+  } else if (operation === "-") {
+    return value1 - value2;
+  } else if (operation === "*") {
+    return value1 * value2;
+  } else if (operation === "/") {
+    return value1 / value2;
+  } else return 0;
+}
+
+// I feel like there might've been an easier/more efficient way to solve this problem but I wanted to do it the way I knew how. I will do more research on how others solved this problem to gain a better understanding on the different ways to write this specific function.
